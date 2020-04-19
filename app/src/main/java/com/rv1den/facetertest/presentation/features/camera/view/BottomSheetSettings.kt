2c232338096802a6482adb97a2ac7fb2cc033744
Paul Karpukhin <ruberoid55@gmail.com>
2020-04-19 16:13:43 +0600

    added settings button

---
+++ app/src/main/java/com/rv1den/facetertest/presentation/features/camera/view/BottomSheetSettings.kt
@@ -0,0 +1,7 @@
+package com.rv1den.facetertest.presentation.features.camera.view
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetSettings: BottomSheetDialogFragment() {
+
+}
